56ef08d48f102320875d5a3fb3c5a0f5e5afdb45
Micla <micol.giannelli@outlook.com>
2026-01-10 23:39:08 +0000

    renamed folder removed submit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,98 @@
+import Board from "./components/Board"; // capitalize to match file name
+import NewTicketForm from "./components/NewTicketForm";
+import "./App.css";
+import { Routes, Route, useNavigate} from "react-router-dom";
+import { useState, useEffect } from "react";
+import type { ColumnType, TicketType, TicketFormData } from "./types";
+
+function App() {
+  // const [boardData, setBoardData] = useState<BoardType | null>(null);
+  const navigate = useNavigate();
+  const [columns, setColumns] = useState<ColumnType[]>([]);
+  const [tickets, setTickets] = useState<TicketType[]>([]);
+
+  useEffect(() => {
+    // id: "12345",
+    // title: "Micol Board",
+    const mockTickets: TicketType[] = [
+      { id: "h234567", title: "Design Data Model", columnID: "234567" },
+      { id: "h209765", title: "Build React App", columnID: "234567" },
+      { id: "h23ertyui7", title: "Build Backend", columnID: "234567" },
+      { id: "90734567", title: "Project Set Up", columnID: "2dlkjgas7" },
+      { id: "90709765", title: "Architecure plan", columnID: "2dlkjgas7" },
+    ];
+
+    // Unsure about the getting the coloumns
+    const mockColumns: ColumnType[] = [
+      { id: "234567", title: "TO-DO" },
+      { id: "2dlkjgas7", title: "IN PROGRESS" },
+      { id: "ABCD", title: "DONE" },
+    ];
+
+    setColumns(mockColumns);
+    setTickets(mockTickets);
+  }, []); // empty list here means effect happens only on mount
+
+  if (!columns) {
+    return <div>Loading...</div>;
+  }
+
+  // const moveTicket = (ticetId, columnId) => {
+  //   // make post request ???
+  //   setBoardData( (prevBoardData) => {
+
+  //   }
+  // }
+
+  // TODO: Fix syntax
+  // TODO: add logic to re introduce ticket in backend and look up how this should be done
+  //   HANDLE NEW TICKET SHOULD NOW HAPPEN ON BOARD SUBMISSION
+  const handleNewTicket = async (newTicketData: TicketFormData) => {
+    // pass in value of form here
+    // should this be the saame function to edit a ticket?
+    // For now this is a basic function with no optimisitc updates
+    console.log("newTicketData", newTicketData);
+    
+    // const response = await fetch(`{}tickets`, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(newTicketData),
+    // });
+
+    // // Set new Global State for tickets
+    // setTickets((prevTickets) => {
+    //   return [
+    //     ...prevTickets,
+    //     {
+    //       id: crypto.randomUUID(),
+    //       title: newTicketData.title,
+    //       columnID: "234567", // default to TO-DO column
+    //     },
+    //   ];
+    // });
+
+    // Navigate back to board view
+    navigate("/");
+  };
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route
+          path="/"
+          element={<Board columns={columns} tickets={tickets}></Board>}
+        />
+        <Route
+          path="/new-ticket"
+          element={
+            <NewTicketForm handleSubmit={handleNewTicket}></NewTicketForm>
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Comment.tsx
@@ -0,0 +1,13 @@
+// Board.jsx
+
+export default function Board() {
+  return (
+    <div className="board">
+      empty content
+      {/* Render three columns */}
+      {/* <Column title="To Do" />
+      <Column title="In Progress" />
+      <Column title="Done" /> */}
+    </div>
+  );
+}
