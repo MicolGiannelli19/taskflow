5aff38e2e3a078432a4ff4744051a6abd44103ca
Micla <micol.giannelli@outlook.com>
2026-04-14 08:33:47 +0100

    tweeks

--- frontend/src/pages/Board/componets/Ticket.tsx
+++ frontend/src/pages/Board/componets/Ticket.tsx
@@ -1,17 +1,18 @@
 // Board.jsx
 import styles from "./Ticket.module.css";
-import type { TicketTypeSmall } from "../types";
+import type { TicketTypeSmall } from "../../../types";
 
 interface TicketProps {
   ticket: TicketTypeSmall;
+  onMove?: () => void;
 }
 
-export default function Ticket({ ticket }: TicketProps) {
+export default function Ticket({ ticket, onMove }: TicketProps) {
   return (
     <div className={styles.ticket}>
       <header>
         <h1>{ticket.title}</h1>
-        <button>move</button>
+        <button onClick={onMove} disabled={!onMove}>move →</button>
       </header>
       {/* <p>{description || "Lorem ipsum..."}</p> */}
     </div>
